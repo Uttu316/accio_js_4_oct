d0c2cc20145aa551f126a1c11f6c3f13ad8c64a8
Utkarsh Gupta <Utkarsh.Gupta@setindia.com>
2024-10-20 22:28:33 +0530

    modal for image

--- Users/scripts/main.js
+++ Users/scripts/main.js
@@ -16,10 +16,10 @@ const getUsers = async () => {
 };
 
 const create_card = (cardData) => {
-  const { name, email, phone, age, gender, photo } = cardData;
+  const { name, email, large, phone, age, gender, photo } = cardData;
   const card = $(`<div class="card">
         <div class="photo">
-          <img src=${photo} alt=${name} />
+          <img  src=${photo} alt="${name}" />
         </div>
 
         <div class="details">
@@ -35,10 +35,33 @@ const create_card = (cardData) => {
   return card;
 };
 
+const onImgClick = (event) => {
+  const { target } = event;
+
+  const { src, alt } = target;
+  const body = $("body");
+
+  body.addClass("no_scroll");
+
+  const modal = $("#modal");
+  const modalImg = modal.find("img")[0];
+  console.log({ modalImg, src });
+  modalImg.src = src;
+  modalImg.alt = alt;
+
+  modal.fadeIn();
+  event.stopPropagation();
+};
+
+const onCardClick = (event) => {
+  const { target } = event;
+  console.log(target);
+};
+
 const showUserDetails = (user) => {
   const { name, email, phone, gender, dob, picture } = user;
   const { title, first, last } = name;
-  const { thumbnail } = picture;
+  const { thumbnail, large } = picture;
   const { age } = dob;
 
   let fullname = title + " " + first + " " + last;
@@ -50,9 +73,28 @@ const showUserDetails = (user) => {
     photo: thumbnail,
     age,
     gender,
+    large,
   };
   const card = create_card(cardData);
+  const img = $(card).find("img");
+
+  img.on("click", onImgClick);
+
+  card.on("click", onCardClick);
+
   return card;
 };
 
+const registerModalClose = () => {
+  const modalClose = $("#modal_close_btn");
+  modalClose.on("click", (e) => {
+    const modal = $("#modal");
+    const body = $("body");
+
+    body.removeClass("no_scroll");
+    modal.fadeOut();
+  });
+};
+
 getUsers();
+registerModalClose();
