33dc79ea7615c41ceb6efafe4f4db396297a363f
Utkarsh Gupta <Utkarsh.Gupta@setindia.com>
2024-10-23 10:10:03 +0530

    modules

---
+++ modules/a.js
@@ -0,0 +1,9 @@
+const x = () => {
+  console.log("HEllo");
+};
+
+export const userObj = {
+  name: "Utkarsh",
+};
+
+export default x;
---
+++ modules/b.js
@@ -0,0 +1,8 @@
+import AX, { userObj } from "./a.js";
+AX();
+
+console.log(userObj);
+
+const userObj = {
+  name: "Sujata",
+};
