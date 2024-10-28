540210053efccf443c7719f38864df77364bcf6a
Utkarsh Gupta <Utkarsh.Gupta@setindia.com>
2024-10-28 23:12:43 +0530

    Hooks

--- myFirstApp/src/components/form/button.jsx
+++ myFirstApp/src/components/form/button.jsx
@@ -4,14 +4,14 @@
 import styles from "./form.module.css";
 
 
-const Button = React.forwardRef((props,ref)=>{
+const Button = (props)=>{
     const {onClick,label,style} = props;
    
     return (
-        <button ref={ref} onClick={onClick} className={style|| styles.submitBtn}>
+        <button onClick={onClick} className={style|| styles.submitBtn}>
             {label}
           </button>
     )
-})
+}
 
 export default Button
